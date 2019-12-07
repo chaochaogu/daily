6a15c4f7f7b337fadb006b151418a04de0338745
gcc <723454695@qq.com>
2019-12-07 22:55:50 +0800

    add

---
+++ src/main/java/com/chaochaogu/ArraysAbout.java
@@ -0,0 +1,15 @@
+package com.chaochaogu;
+
+import java.util.Arrays;
+
+/**
+ * @author chaochao gu
+ * @date 2019/12/7
+ */
+public class ArraysAbout {
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3};
+        Integer[] copy = Arrays.copyOf(array, 4);
+
+    }
+}
