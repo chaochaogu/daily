b1912b6b2f979c3651aba9c32a8a46423147124c
chaochao.gu <723454695@qq.com>
2019-12-25 13:26:02 +0800

    add thread

---
+++ src/main/java/com/chaochaogu/ThreadAbout.java
@@ -0,0 +1,17 @@
+package com.chaochaogu;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author chaochao Gu
+ * @date 2019/12/25
+ */
+public class ThreadAbout {
+    public static void main(String[] args) {
+        Runnable task = () -> System.out.println("run...");
+        Executor executor = Runnable::run;
+        Executor executor1 = (t) -> new Thread(t).start();
+        executor.execute(task);
+        executor1.execute(task);
+    }
+}
