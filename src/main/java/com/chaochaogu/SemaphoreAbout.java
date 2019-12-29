6fc376bab3aca64c91cd8ca72eb6ed45d71bd3f0
gcc <723454695@qq.com>
2019-12-29 23:00:53 +0800

    add

---
+++ src/main/java/com/chaochaogu/SemaphoreAbout.java
@@ -0,0 +1,15 @@
+package com.chaochaogu;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author chaochao gu
+ * @date 2019/12/29
+ */
+public class SemaphoreAbout {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(0);
+        semaphore.release();
+        System.out.println(semaphore.availablePermits());
+    }
+}
