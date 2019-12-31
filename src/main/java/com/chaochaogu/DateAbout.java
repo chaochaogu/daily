def0aa71a513906cdfcd0fbdd66c99a95ead9624
chaochao.gu <723454695@qq.com>
2019-12-31 15:37:05 +0800

    add

--- src/main/java/com/chaochaogu/DateAbout.java
+++ src/main/java/com/chaochaogu/DateAbout.java
@@ -1,7 +1,10 @@
 package com.chaochaogu;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * @author chaochao Gu
@@ -12,5 +15,8 @@ public class DateAbout {
         LocalDate date = LocalDate.parse("2019-12-30");
         System.out.println(DateTimeFormatter.ofPattern("yyyy-MM-dd").format(date));
         System.out.println(DateTimeFormatter.ofPattern("YYYY-MM-dd").format(date));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+        LocalDateTime localDateTime = LocalDateTime.from(formatter.parse("201912311420"));
+        Date fromDate = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
