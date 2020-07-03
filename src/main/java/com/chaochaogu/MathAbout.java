eabc7102414f650ada4227d7eec405534dba7521
chaochao.gu <723454695@qq.com>
2020-07-03 17:27:56 +0800

    add math round

---
+++ src/main/java/com/chaochaogu/MathAbout.java
@@ -0,0 +1,11 @@
+package com.chaochaogu;
+
+/**
+ * @author chaochao Gu
+ * @date 2020/7/3
+ */
+public class MathAbout {
+    public static void main(String[] args) {
+        System.out.println(Math.round(10 /4));
+    }
+}
