a26b98f038abf4f6c136cda2431363ec19c0f2b5
chaochao.gu <723454695@qq.com>
2020-08-13 10:57:12 +0800

    add

--- src/main/java/com/chaochaogu/GuavaAbout.java
+++ src/main/java/com/chaochaogu/GuavaAbout.java
@@ -2,6 +2,8 @@ package com.chaochaogu;
 
 import com.google.common.collect.Maps;
 
+import java.util.Map;
+
 /**
  * @author chaochao Gu
  * @date 2019/12/5
@@ -9,5 +11,7 @@ import com.google.common.collect.Maps;
 public class GuavaAbout {
     public static void main(String[] args) {
         System.out.println(Maps.newHashMap().isEmpty());
+        Map<Long/*shipmentId*/, Map<Long/*skuOrderId*/, Integer/*refundNum*/>> shipmentRefundMap = Maps.newHashMap();
+        System.out.println(shipmentRefundMap.size());
     }
 }
