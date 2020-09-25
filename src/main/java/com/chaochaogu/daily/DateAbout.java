710ebb40bc52ef1a87a7a7d4f2b24661025f8bec
chaochao.gu <723454695@qq.com>
2020-09-25 09:51:07 +0800

    add time sort

--- src/main/java/com/chaochaogu/daily/DateAbout.java
+++ src/main/java/com/chaochaogu/daily/DateAbout.java
@@ -1,12 +1,16 @@
 package com.chaochaogu.daily;
 
+import com.google.common.collect.Lists;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author chaochao Gu
@@ -27,5 +31,16 @@ public class DateAbout {
         jodaTime.isAfterNow();
         jodaTime.isBeforeNow();
         jodaTime.plusHours(3);
+        org.joda.time.format.DateTimeFormatter jodaFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        DateTime time1 = jodaFormatter.parseDateTime("2020-09-23 11:23:34");
+        DateTime time2 = jodaFormatter.parseDateTime("2020-09-23 11:24:34");
+        DateTime time3 = jodaFormatter.parseDateTime("2020-09-23 11:26:34");
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        List<String> times = Lists.newArrayList();
+        times.add("2020-09-23 11:24:34");
+        times.add("2020-09-23 11:26:34");
+        times.add("2020-09-23 11:23:34");
+        times.sort((v1, v2) -> LocalDateTime.from(pattern.parse(v2)).compareTo(LocalDateTime.from(pattern.parse(v1))));
+        System.out.println(times);
     }
 }
